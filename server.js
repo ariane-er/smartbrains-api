aa067d5c7a2c915457dd5a5f31df4828cc978bbb
Ariane <ariane.ernandorena@nexusedge.co>
2020-06-27 12:30:51 -0500

    debug get

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const profile = require('./controllers/profile.js');
 const image = require('./controllers/image');
 
 // Connect to database
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 const db = knex({
     client: 'pg',
     connection: {
