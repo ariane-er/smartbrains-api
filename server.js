a898a78edcc689a3760d3307a2562b34a5ba8017
Ariane <ariane.ernandorena@nexusedge.co>
2020-06-27 11:13:47 -0500

    Fixed ports1

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put("/image", (req, res) => { image.handleImage(req, res, db)});
 app.post("/imageurl", (req, res) => { image.handleAPICall(req, res)})
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`)
 });
